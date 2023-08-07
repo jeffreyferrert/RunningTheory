227040fd4092df8c5b68d8cdd1ea9b8c6f071b6c
JasonStaubach <jbstaubach@gmail.com>
2023-08-07 09:22:57 -0400

    more comment work

--- backend/routes/api/comments.js
+++ backend/routes/api/comments.js
@@ -4,13 +4,20 @@ const mongoose = require('mongoose');
 
 const validateCommentInput = require('../../validations/comments')
 
-    router.post('/tracks/comments', validateCommentInput, (req, res) => {
-      Comment.create(req.body).then((comment) => {
-        console.log(comment)
-        res.redirect(`/reviews/${comment.reviewId}`);
-      }).catch((err) => {
-        console.log(err.message);
-      });
+    router.post('/comments', validateCommentInput, async (req, res, next) => {
+      try {
+        const newComment = new Track({
+          author: req.user._id,
+          track: req.track_id,
+          description: req.body.description
+        });
+    
+        let track = await newComment.save();
+        return res.json(track);
+      }
+      catch(err) {
+        next(err);
+      }
     });
 
     router.get('/comments',  (req, res) => {
@@ -22,6 +29,25 @@ const validateCommentInput = require('../../validations/comments')
           createdAt: req.comment.createdAt
         });
       });
+
+    router.patch('/comments', async (req, res, next) => {
+      if(!req.user) return res.json(null)
+      try {
+        const newComment = new Track({
+          author: req.user._id,
+          track: req.track_id,
+          description: req.body.description
+        });
+    
+    
+        let track = await newTrack.save();
+        track = await track.populate('author', '_id username');
+        return res.json(track);
+      }
+      catch(err) {
+        next(err);
+      }
+    })
       
 
     module.exports = router;
