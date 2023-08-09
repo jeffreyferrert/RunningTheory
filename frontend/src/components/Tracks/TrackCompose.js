c66a0b08a752b25527e55a18ef115713ce0b4077
Juan A <avilajajuan@gmail.com>
2023-08-09 10:21:27 -0400

    styling

--- frontend/src/components/Tracks/TrackCompose.js
+++ frontend/src/components/Tracks/TrackCompose.js
@@ -99,8 +99,8 @@ function TrackCompose() {
                             required
                         />
                         </div>
-                        <button id='' className='form-box' onClick={handleClick}>Check Address</button>
-                        <button  className='form-box'onClick={handleReset}>Rest Address</button>
+                        <button  className='form-box-button' onClick={handleClick}>Check Address</button>
+                        <button  className='form-box-button'onClick={handleReset}>Rest Address</button>
                         <div >Input The Approximate Distance
                         <input
                             className='form-box'
