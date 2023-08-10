a18ebfc75ba0165aeef5e7f5f729c1f43fe506c5
JasonStaubach <jbstaubach@gmail.com>
2023-08-10 15:49:54 -0400

    making user show page

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -25,45 +25,50 @@ function Profile() {
   //   return <div>{currentUser.username} has no Tracks</div>;
   // } else {
   return (
-    <div className="profile-container">
-      <img id='user-img' alt='hihi-img' src='/UserBackSplash.jpg'></img>
-      {/* <div className='user-header'>
-        <h2 className='welcome-user'>{currentUser.username}'s Stats and Tracks</h2>
-      </div> */}
-      <div className="profile-times-outer">
-        <h2>All of {currentUser.username}'s Times</h2>
-        {userTimes.length === 0 ? (
-          <p className="no-race-message">Go out and run your first race!</p>
-        ) : (
-          <div className="profile-times-inner">
-            {userTimes && (userTimes.map((time, index) => (
-              <li className={index}>
-                <Time key={index} time={time} currUser={currentUser} />
+    <>
+      <div className='user-header'>
+        <div className="profile-demo">
+          <p className='single-letter'>{currentUser.username[0].toUpperCase()}</p>
+        </div>
+        <h2 className='welcome-user'>{currentUser.username}'s stats and tracks</h2>
+      </div>
+      <div className="profile-container">
+        <img id='user-img' alt='hihi-img' src='/UserBackSplash.jpg'></img>
+        <div className="profile-times-outer">
+          <h2>All of {currentUser.username}'s Times</h2>
+          {userTimes.length === 0 ? (
+            <p className="no-race-message">Go out and run your first race!</p>
+          ) : (
+            <div className="profile-times-inner">
+              {userTimes && (userTimes.map((time, index) => (
+                <li className={index}>
+                  <Time key={index} time={time} currUser={currentUser} />
 
-              </li>
-            )))
-            }
-          </div>
-        )}
+                </li>
+              )))
+              }
+            </div>
+          )}
+        </div>
+        <div className="profile-tracks-outer">
+          <h2>All of {currentUser.username}'s Tracks</h2>
+          {userTracks.length === 0 ? (
+            <p className="no-race-message">Make a race for everyone to see!</p>
+          ) : (
+            <div className="profile-tracks-inner">
+              {
+                userTracks.map(track => (
+                  <TrackBox
+                    key={track._id}
+                    track={track}
+                  />
+                ))
+              }
+            </div>
+          )}
+        </div>
       </div>
-      <div className="profile-tracks-outer">
-        <h2>All of {currentUser.username}'s Tracks</h2>
-        {userTracks.length === 0 ? (
-          <p className="no-race-message">Make a race for everyone to see!</p>
-        ) : (
-          <div className="profile-tracks-inner">
-            {
-              userTracks.map(track => (
-                <TrackBox
-                  key={track._id}
-                  track={track}
-                />
-              ))
-            }
-          </div>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
 
