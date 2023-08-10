15fbff756acad4f9a6eaf590c2673d1e1c49cf91
jeffreyferrert <jeffrey.ferrert@gmail.com>
2023-08-10 13:41:50 -0400

    add and rest time

--- frontend/src/components/TrackShowPage/Time.js
+++ frontend/src/components/TrackShowPage/Time.js
@@ -1,51 +1,32 @@
-import React from "react"
-import { useDispatch } from "react-redux"
-import { useState } from "react"
-import { useParams } from "react-router-dom/cjs/react-router-dom"
-import { deleteTime } from "../../store/times"
-import './Time.css'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { deleteTime } from "../../store/times";
+import './Time.css';
 
 export default function Time({ time, currUser }) {
-    const dispatch = useDispatch()
-    const { trackId } = useParams()
-    // console.log(time.track._id)
-    // console.log(trackId)
-    function handleSubmit(e) {
-        dispatch(deleteTime(time._id))
-    }
+    const dispatch = useDispatch();
+    const { trackId } = useParams();
+
+    const formattedTime = (value) => (value < 10 ? "0" + value : value);
+
+    const handleDelete = () => {
+        dispatch(deleteTime(time._id));
+    };
+
+    const isCurrentUser = time.author._id === currUser._id;
 
     return (
-        time.track._id === trackId ? (
-            <div className="single-time-container">
-                <p>{time.author.username}</p>
-                <p className="track-time">
-                    {time.hours < 10 ? "0" + time.hours : time.hours}:
-                    {time.minutes < 10 ? "0" + time.minutes : time.minutes}:
-                    {time.seconds < 10 ? "0" + time.seconds : time.seconds}
-                </p>
-                {time.author._id === currUser._id ? (
-                    <form onSubmit={(e) => handleSubmit(e)}>
-                        <input id="delete-time-btn"
-                        type="submit" value={`Remove Time`} />
-                    </form>
-                ) : null}
-            </div>
-        ) : (
-            <div className="single-time-container">
-                <p>{time.track.name}</p>
-                <p className="track-time">
-                    {time.hours < 10 ? "0" + time.hours : time.hours}:
-                    {time.minutes < 10 ? "0" + time.minutes : time.minutes}:
-                    {time.seconds < 10 ? "0" + time.seconds : time.seconds}
-                </p>
-                {time.author._id === currUser._id ? (
-                    <form onSubmit={(e) => handleSubmit(e)}>
-                        <input id="delete-time-btn"
-                        type="submit" value={`Remove Time`} />
-                    </form>
-                ) : null}
-            </div>
-        )
-    )
-
+        <div className="single-time-container">
+            <p>{time.track._id === trackId ? time.author.username : time.track.name}</p>
+            <p className="track-time">
+                {formattedTime(time.hours)}:{formattedTime(time.minutes)}:{formattedTime(time.seconds)}
+            </p>
+            {isCurrentUser && (
+                <form onSubmit={(e) => { handleDelete(); }}>
+                    <input id="delete-time-btn" type="submit" value={`Remove Time`} />
+                </form>
+            )}
+        </div>
+    );
 }
