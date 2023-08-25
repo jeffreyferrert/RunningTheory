b009a76bffa43c762c838fe69e200f9afab9fd42
JasonStaubach <jbstaubach@gmail.com>
2023-08-25 01:56:55 -0400

    fixed my aboutMe links

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -68,7 +68,7 @@ function AboutUs() {
                 <div id='extender-container-signin'>
                   <div id='icons'>
                     <div className='symbol'><a href='https://github.com/JasonStaubach'><i className="fa-brands fa-github fa-2xl" style={{ fontSize: '3em', color: "#000000" }}></i></a></div>
-                    <div id='linked' className='symbol'> <a href=''><i className="fa-brands fa-linkedin fa-2xl" style={{ fontSize: '3em', color: "#0a66c2" }}></i></a></div>
+                    <div id='linked' className='symbol'> <a href='https://www.linkedin.com/in/jason-staubach-80313a22b/'><i className="fa-brands fa-linkedin fa-2xl" style={{ fontSize: '3em', color: "#0a66c2" }}></i></a></div>
                   </div>
                 </div>
               </div>
